167d263d8a165b925e798c6f92cf2ff8ba34b8ad
k0ccc <45402819+k0ccc@users.noreply.github.com>
2022-05-29 15:51:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 add = 50
 offset = 0
 limit = add
-channelsTemp = []
 channels = []
 someMath = 0 
 
@@ -28,27 +27,5 @@ print("Create CSV 'data'")
 data = pd.DataFrame(channels)
 data.to_csv("./data.csv")
 
-
-# columns = ['t', 'u', 'pc', 'l', 'a', 'i', 'p', 'baa', 'baa', 'baa']
-
-
-# with file:
-#     writer.writeheader()
-#     # print(channels)
-#     for i in channels:
-#         # a = i.split( "},{")
-#         print(i)
-#         # for z in a:
-#         #     print(z)
-#             # writer.writerow([a,u])
-
-
 print("Writing complete")
-# while true:
-#     offset =+ add;
-#     limit =+ add;
-#     res = requests.get("https://combot.org/api/chart/all?limit=" + str(10) + "&offset=" + str(offset))
-#     if (res.text == '[]'):
-#         break
-#     else:
-#         channels.append(res.text)
+
